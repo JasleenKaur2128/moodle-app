ab3e8641c3bd14761d0826246152e9da5eaee5db
Jasleen <jasleen2357@gmail.com>
2025-01-29 15:31:37 -0800

    Add styling for card component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
 import data from "../../data/data.json";
+import "./Cards.scss";
 
 function Cards() {
   const [users, setUsers] = useState(data);
-  console.log(users)
+  console.log(users);
   return (
-    <section>
-      <h4>Card List</h4>
-      {users && users.map((user) => {
-        return(
-            <article key={user.id}>
-                <img src={user.avatar} alt={user.id} />
-                <h2>{user.first_name}</h2>
-                <h3>{user.email}</h3>
+    <section className="cards-container">
+      {users &&
+        users.map((user) => {
+          return (
+            <article key={user.id} className="card">
+              <img src={user.avatar} alt={user.id} className="card__img" />
+              <div className="card__info">
+                <h2 className="card__title">{user.first_name}</h2>
+                <h3 className="card__email">{user.email}</h3>
+              </div>
             </article>
-        )
-      })}
+          );
+        })}
     </section>
   );
 }
